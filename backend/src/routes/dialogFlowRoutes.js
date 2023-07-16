f64902e4f34b8061501d3ebc6413f44a734c1970
laujuu <laurafayec@gmail.com>
2023-07-15 22:29:48 -0300

    feat: chatbot module, eventQuery

--- backend/src/routes/dialogFlowRoutes.js
+++ backend/src/routes/dialogFlowRoutes.js
@@ -1,31 +1,15 @@
 const { Router } = require('express');
-const dialogflow = require('dialogflow');
-const config = require('../config/keys');
-
-const sessionClient = new dialogflow.SessionsClient();
-
-const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
-
 const dialogFlowRoutes = Router();
+const chatbot = require('../chatbot/chatbot')
 
 dialogFlowRoutes.post('/api/df_text_query', async (req, res) => {
-    const request = {
-        session: sessionPath,
-        queryInput: {
-            text: {
-                text: req.body.text,
-                languageCode: config.dialogFlowSessionLanguageCode
-            }
-        }
-    };
-    let responses = await sessionClient
-        .detectIntent(request);
-
+    let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
     res.send(responses[0].queryResult)
 });
 
-dialogFlowRoutes.post('/api/df_event_query', (_req, res) => {
-    res.send({'do': 'event query'});
+dialogFlowRoutes.post('/api/df_event_query', async (req, res) => {
+    let responses = await chatbot.eventQuery(req.body.event, req.body.parameters);
+    res.send(responses[0].queryResult)
 });
 
 module.exports = dialogFlowRoutes;
