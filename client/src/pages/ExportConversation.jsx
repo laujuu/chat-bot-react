abf646a25aebebae97166958b61b798071e74c53
laujuu <laurafayec@gmail.com>
2023-07-18 18:04:46 -0300

    feat: change api link to deploy

--- client/src/pages/ExportConversation.jsx
+++ client/src/pages/ExportConversation.jsx
@@ -8,7 +8,7 @@ class ExportConversation extends React.Component {
         <div className={styles.container}>
           <h1 className={styles.title}>Export Bot Conversations</h1>
           <button style={{height: '100px'}} className='btn waves-effect  blue darken-3 pulse'>
-            <a style={{ color: 'white', fontSize: '30px' }} href="http://localhost:5001/exportConversation" target="_blank" rel="noopener noreferrer">Export Conversation</a>
+            <a style={{ color: 'white', fontSize: '30px' }} href="https://laura-chatbot.onrender.com/exportConversation" target="_blank" rel="noopener noreferrer">Export Conversation</a>
           </button>
         </div>
       </main>
